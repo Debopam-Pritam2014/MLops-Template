2d0606d2392f63473868d5470d28fbc48ee5acde
Debopam_Dey <djdebdj172@gmail.com>
2024-09-20 19:27:51 +0530

    setup.py file created

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description=f.read()
+
+__version__="0.0.1"
+
+
+REPO_NAME="MLops-Template"
+AUTHOR_NAME="Debopam-pritam2014"
+SRC_REPO="First-project"
+AUTHOR_EMAIL='debopamdeycse19@gmail.com'
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_NAME,
+    author_email=AUTHOR_EMAIL,
+    long_description="MLOps Template for everyone.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src"),
+
+)
